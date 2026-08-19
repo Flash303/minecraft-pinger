37e8b1bb4e0991e67ab8eec6a31ce1b3032ea26a
Flash <Flash303mc@gmail.com>
2026-08-19 13:13:24 +0200

    fix: use cloudflare servers

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ pub mod bedrock;
 
 use error::PingError;
 use hickory_resolver::Resolver;
+use hickory_resolver::config::{CLOUDFLARE, ResolverConfig};
 use hickory_resolver::net::runtime::TokioRuntimeProvider;
 use std::sync::Arc;
 
@@ -18,6 +19,20 @@ pub struct MinecraftPinger {
 
 impl MinecraftPinger {
     pub fn new() -> Result<Self, PingError> {
+        let resolver = Resolver::builder_with_config(
+            ResolverConfig::udp_and_tcp(&CLOUDFLARE),
+            TokioRuntimeProvider::default()
+        )
+            .build()
+            .map_err(|e| PingError::Init(e.to_string()))?;
+
+        Ok(Self {
+            dns_resolver: Arc::new(resolver)
+        })
+    }
+
+    #[deprecated(note="Some problems with default DNS of OVH for some servers.")]
+    pub fn new_legacy() -> Result<Self, PingError> {
         let builder = Resolver::builder_tokio()
             .map_err(|e| PingError::Init(e.to_string()))?;
         let resolver = builder.build()
